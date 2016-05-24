107541da985755979d37951b3f71fb47e6398c0b
Erik Petigura <petigura@caltech.edu>
2016-05-23 17:09:22 -0700

    added unit test

--- tests/test_ganymede.sh
+++ tests/test_ganymede.sh
@@ -5,8 +5,8 @@ umask u=rwx,g=rx,o=rx # Change the appropriate file permissions to read only
 set -x # Want to keep tabs on values of variables
 cd ${SM_DIR}
 export SM_PROJDIR=${SM_HTML_DIR}
-python code/py/scripts/restwav_batch.py -f --obs ${OBS}
-python code/py/scripts/specmatch_batch.py ${OBS} --np 8 ${DEBUG}
-python code/py/scripts/add_telluric.py ${OBS} --plot
-python code/py/scripts/add_polish.py ${OBS} fm
-python code/py/scripts/smplots_batch.py --panels --matches-chi --polish --quicklook ${OBS}
+python bin/restwav_batch.py -f --obs ${OBS}
+#python code/py/scripts/specmatch_batch.py ${OBS} --np 8 ${DEBUG}
+#python code/py/scripts/add_telluric.py ${OBS} --plot
+#python code/py/scripts/add_polish.py ${OBS} fm
+#python code/py/scripts/smplots_batch.py --panels --matches-chi --polish --quicklook ${OBS}
