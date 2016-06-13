b2dbd2cd20497fa1332f19b3ddfc7ed5e0917620
Erik Petigura <petigura@caltech.edu>
2016-06-13 16:32:12 +0100

    bugfix: fixed rot() kernel

--- smsyn/kernels.py
+++ smsyn/kernels.py
@@ -1,3 +1,5 @@
+"""Module for different broadening kernels.
+"""
 from scipy.special import jn
 from scipy import integrate
 import numpy as np
@@ -18,21 +20,22 @@ def rotmacro_ft(sigma, xi, vsini, u1=0.43, u2=0.31, intres=100):
         array: Fourier transform of rot-macro profile at sig
     """ 
     
-    tarr = np.outer(np.ones(sigma.shape[0]),np.linspace(0,1,intres)).transpose()
+    tarr = np.outer( np.ones(sigma.shape[0]), np.linspace(0, 1, intres) )
+    tarr = tarr.transpose()
 
     t1 = (
-        (1-u1*(1-np.sqrt(1-tarr**2)) - 
-         u2*(1-np.sqrt(1-tarr**2))**2)/(1-u1/3-u2/6)
+        (1 - u1 * ( 1 - np.sqrt(1 - tarr**2) ) - 
+         u2 * ( 1 - np.sqrt( 1 - tarr**2 ) )**2) / ( 1 - u1 / 3 - u2 / 6 )
     )
     t2 = (
-        (np.exp(-np.pi**2 * xi**2 * sigma**2 * (1-tarr**2)) + 
-        np.exp(-np.pi**2 * xi**2 * sigma**2 * tarr**2)) * 
+        ( np.exp( -np.pi**2 * xi**2 * sigma**2 * (1-tarr**2) ) + 
+          np.exp( -np.pi**2 * xi**2 * sigma**2 * tarr**2) ) * 
         jn(0,2*np.pi*sigma*vsini*tarr) * tarr
     )
 
     m = t1 * t2
-    Mt = integrate.trapz(m,dx=np.mean(tarr[1:] - tarr[0:-1]),axis=0)
-    return Mt
+    kernel_ft = integrate.trapz(m,dx=np.mean(tarr[1:] - tarr[0:-1]),axis=0)
+    return kernel_ft
 
 def rotmacro(n, dv, xi, vsini, **kwargs):
     """Rotational-Macroturbulent Broadening Kernel
@@ -45,27 +48,24 @@ def rotmacro(n, dv, xi, vsini, **kwargs):
         **kwargs: passed to rotmacro_ft
 
     Returns:
-        varr (array): velocities
-        M (array): broadening kernel (same size as varr)
+        array: velocities
+        array: broadening kernel (same size as varr)
     """
-    nind = (n+1)/2 # number of independent points
-    vmax = dv*nind
-    varr = np.linspace(-vmax,vmax,n)
+    nind = (n + 1) / 2 
+    vmax = dv * nind
+    varr = np.linspace( -vmax, vmax, n)
 
     # Fourier transform frequencies, for given velocity displacements
     sigarr = fft.fftfreq(n,d=dv)
     sigarr = sigarr[:nind]
+    kernel_ft = rotmacro_ft(sigarr,xi,vsini,**kwargs)
+    kernel_ft = np.hstack([ kernel_ft, kernel_ft[1:][::-1] ])
+    kernel = fft.ifft(kernel_ft)
+    kernel = fft.fftshift(kernel)
+    kernel = kernel/np.sum(kernel)
+    return varr,kernel
 
-    #M_ft_ep = np.array([rotmacro_ft(sig,xi,vsini,**kwargs) for sig in sigarr])
-    M_ft = rotmacro_ft(sigarr,xi,vsini,**kwargs)
-
-    M_ft = np.hstack([ M_ft, M_ft[1:][::-1] ])
-    M = fft.ifft(M_ft)
-    M = fft.fftshift(M)
-    M = M/np.sum(M)
-    return varr,M
-
-def rot(dv, vsini, u1=0.6):
+def rot(n, dv, vsini, u1=0.6):
     """Rotational Kernel
 
     Create a 1-d convolution kernel to broaden a spectrum from a rotating star
@@ -73,9 +73,10 @@ def rot(dv, vsini, u1=0.6):
     Args:
         deltaV (float): spacing between kernel points (km/s)
         vsini (float): projecte rotational velocoity
+
         u1 (Optional[float]): linear limb-darkening
-            coefficient. default = 0.6 which is typical for
-            photospheric lines.  The specific intensity I at any angle
+            coefficient. Default value of 0.6 is typical for
+            photospheric lines. The specific intensity I at any angle
             theta from the specific intensity Icen at the center of
             the disk is given by:
   
@@ -88,20 +89,19 @@ def rot(dv, vsini, u1=0.6):
 
     Notes:
         Adapted from lsf_rotate.pro by Erik Petigura
+
     """
+    nind = (n + 1) / 2 # number of independent points
+    vmax = dv * nind
+    varr = np.linspace(-vmax, vmax, n).astype(float)
 
     e1 = 2.0 * (1.0 - u1)
     e2 = np.pi * u1 / 2.0
     e3 = np.pi * (1.0 - u1 / 3.0)
-    npts = np.ceil(2.0 * vsini / dv)
-    if (npts % 2)==0:
-        npts = npts +1
 
-    nwid = npts/2.0
-    x = np.arange(npts) - nwid
-    x = x * dv / vsini  
+    x = varr / vsini
     x1 = np.abs(1.0 - x**2)
-    kernel = ((e1 * np.sqrt(x1) + e2*x1)/e3).astype(float)
-    varr = x * vsini
+    kernel = ( ( e1 * np.sqrt(x1) + e2 * x1 ) / e3).astype(float)
+    kernel[np.abs(x) > 1] = 0 # kernel should be 0 for v > vsini 
+    kernel /= kernel.sum() # normalize 
     return varr, kernel
-   
