d6e8b30e96a8df20ec45f82a9f62f9af4ef5eee1
BJ Fulton <bjfultn@gmail.com>
2016-06-21 14:08:17 -1000

    mocking modules

--- doc/conf.py
+++ doc/conf.py
@@ -287,3 +287,15 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas', 'h5py']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
